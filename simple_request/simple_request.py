7d3765188f8930d750a696583a5c95491c551d3d
lu-tar <l.tar@protonmail.com>
2025-01-23 20:28:32 +0100

    destination log

--- simple_request/simple_request.py
+++ simple_request/simple_request.py
@@ -13,10 +13,10 @@ if result.stderr:
     print("Errori:", result.stderr)
           
 r = requests.get('https://updater.factorio.com/get-available-versions')
-with open("semaphore_python.log", "w") as file:
+with open("/luca/home/factorio-updater/semaphore_python.log", "w") as file:
         file.write(now + " " + str(r))
 
-result = subprocess.run(['cat', 'semaphore_python.log'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+result = subprocess.run(['cat', '/luca/home/factorio-updater/semaphore_python.log'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 print(result.stdout)
 if result.stderr:
     print("Errori:", result.stderr)
