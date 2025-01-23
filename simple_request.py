722c306087fd0a448e340786f9377b879a25b488
Luca Tarozzi <102428214+lu-tar@users.noreply.github.com>
2025-01-23 17:24:04 +0100

    Update simple_request.py

--- simple_request.py
+++ simple_request.py
@@ -1,8 +1,17 @@
 import requests
+import subprocess
 from datetime import datetime
+
 CLOCK_STR = "%H:%M:%S.%f"
 now = datetime.now().strftime(CLOCK_STR)
+
 print("Testing semaphore python template")
+
+result = subprocess.run(['pip', 'list'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+print(result.stdout)
+if result.stderr:
+    print("Errori:", result.stderr
+          
 r = requests.get('https://updater.factorio.com/get-available-versions')
 print(now + " " + str(r))
 with open("semaphore_python.log", "w") as file:
